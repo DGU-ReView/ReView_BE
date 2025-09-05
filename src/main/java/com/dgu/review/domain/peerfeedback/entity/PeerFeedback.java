5914a1e79597db6b23932b127bb5c2590a2664e4
Mingle31 <happyminji29@gmail.com>
2025-09-05 19:13:58 +0900

    [Feat]커뮤니티, 사용자, 타인평가 엔티티 구현

---
+++ src/main/java/com/dgu/review/domain/peerfeedback/entity/PeerFeedback.java
@@ -0,0 +1,31 @@
+package com.dgu.review.domain.peerfeedback.entity;
+
+import com.dgu.review.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+/*
+    peer_feedback 테이블 매핑
+ */
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+
+@Table(name = "peer_feedback")
+public class PeerFeedback{
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String body;
+
+    @Column(name = "recoding_id", nullable = false, length = 36)
+    private String recodingId;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false,
+    foreignKey = @ForeignKey(name = "fk_pf_user"))
+    private User user;
+}
