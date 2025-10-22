5a10176c87b59fd2f30fc8ab6187cc24a77b6261
yeeun <yeeun@yeeunui-MacBookPro.local>
2025-10-22 19:57:51 +0900

    [Fix] record 문법에 맞게 빌더 어노테이션 위치 변경

--- src/main/java/com/dgu/review/domain/myarchive/dto/RootQuestionCard.java
+++ src/main/java/com/dgu/review/domain/myarchive/dto/RootQuestionCard.java
@@ -3,7 +3,8 @@ package com.dgu.review.domain.myarchive.dto;
 
 import lombok.Builder;
 
+@Builder
 public record RootQuestionCard(
         int order,
         Long questionId
-) { @Builder public RootQuestionCard {} }
+) { }
